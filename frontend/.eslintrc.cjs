31673c765a9fb2e10aae023464a9468b681a0670
Robert Lieblang <roberttheanimal01@gmail.com>
2023-12-15 16:01:49 -0600

    Turn off no-unused-vars rule

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
+    "no-unused-vars": "off",
     quotes: [
       "error",
       "double",
