80bfa6d0632ba1acde41b252a41ea25e2e4410c7
Nicholas Zdrzalka <nicholas.zdrzalka@gmail.com>
2024-02-20 18:03:18 -0600

    Create branch and colors
    Created new branch for ui updates and initialized with color options to use.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      "sm": "440px",
+      "md": "547px",
+      "lg": "768px",
+      "xl": "1024px",
+      "2xl": "1680px",
+    },
+    extend: {
+      colors: {
+        background: "#004643",
+        headline: "#fffffe",
+        paragraph: "#abd1c6",
+        button: "#f9bc60",
+        buttonText: "#001e1d",
+        stroke: "#001e1d",
+        main: "#e8e4e6",
+        highlight: "#f9bc60",
+        secondary: "#abd1c6",
+        tertiary: "#e16162",
+        
+      }
+    },
   },
   plugins: [],
 }
