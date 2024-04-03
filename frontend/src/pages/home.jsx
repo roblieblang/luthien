8be5f8ac0c1ef5ff894f43f9440b9000407e2a1f
Robert Lieblang <roberttheanimal01@gmail.com>
2024-04-03 14:26:32 -0500

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -51,48 +51,46 @@ export default function Home() {
   }, [navigate]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 text-customButtonText overflow-hidden">
-      <div className="text-center space-y-4 max-w-md mx-auto">
-        <h1 className="text-2xl font-extrabold text-customStroke">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 text-customButtonText bg-gray-50">
+      <div className="text-center space-y-4 max-w-lg mx-auto">
+        <h1 className="text-xl sm:text-2xl font-extrabold text-customStroke">
           Seamlessly Sync Your Playlists Between Spotify and YouTube
         </h1>
-        <h2 className="text-lg text-customStroke">
+        <h2 className="text-md sm:text-lg text-customStroke">
           Connect your accounts and let Luthien handle the rest.
         </h2>
       </div>
 
-      <div className="text-center space-y-4 my-8 max-w-md mx-auto">
-        <h3 className="text-xl font-semibold">Why Luthien?</h3>
-        <ul className="space-y-2">
+      <div className="my-8 text-center space-y-4 max-w-lg mx-auto">
+        <h3 className="text-sm sm:text-lg font-semibold">Why Luthien?</h3>
+        <ul className="list-disc list-inside">
           <li>Fast and easy playlist conversion</li>
           <li>Secure connection to your music accounts</li>
           <li>Intuitive design for effortless navigation</li>
         </ul>
       </div>
 
-      <div className="my-6 space-y-4">
-        <div className="flex flex-wrap justify-center items-center space-x-3">
+      <div className="my-6 text-center space-y-4">
+        <div className="flex flex-wrap justify-center items-center text-lg gap-3">
           <FaSpotify className="text-5xl text-green-600" />
           <FaYoutube className="text-5xl text-red-600" />
-          <p className="text-lg">Bring your music together</p>
+          <p>Bring your music together</p>
         </div>
 
-        {isAuthenticated && spotifyAuthStatus && youTubeAuthStatus && (
-          <div className="flex flex-wrap justify-center items-center space-x-2 space-y-4">
-            <YouTubeAuthButton />
-            <Link
-              to="/music"
-              className="flex justify-center items-center px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition-all ease-in-out"
-            >
-              <PiSwap className="text-2xl" />
-              <span className="ml-2">Start Syncing</span>
-            </Link>
-            <SpotifyAuthButton />
-          </div>
-        )}
+        <div className="flex justify-center items-center gap-2 flex-wrap">
+          <YouTubeAuthButton />
+          <Link
+            to="/music"
+            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center justify-center"
+          >
+            <PiSwap size={24} />
+            <span className="ml-2 text-sm">Start Syncing</span>
+          </Link>
+          <SpotifyAuthButton />
+        </div>
       </div>
 
-      <div className="mt-8">
+      <div className="mt-4 mb-10 sm:mb-0">
         <AuthenticationButton />
       </div>
 
