a154723d3cb02b14f508a091bf0f63521ea60121
Robert Lieblang <roberttheanimal01@gmail.com>
2024-04-03 13:42:56 -0500

    Make UI updates required by Google verification
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx
    
    Update home.jsx

--- frontend/src/components/auth0/authenticationButton.jsx
+++ frontend/src/components/auth0/authenticationButton.jsx
@@ -24,7 +24,7 @@ const AuthenticationButton = () => {
         })
       }
     >
-      Log In
+      Get Started
     </button>
   );
 };
--- frontend/src/components/general/footer.jsx
+++ frontend/src/components/general/footer.jsx
@@ -3,22 +3,35 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="bg-gray-600 w-full fixed bottom-0 border-t-2 border-red-500 text-center p-2">
-      <p className="text-sm">© {new Date().getFullYear()} Robert Lieblang</p>
-      <div className="flex justify-center space-x-4 mt-2">
+      <div className="text-white flex justify-center space-x-2 lg:space-x-4 mt-2">
+        <p className="text-xs text-white">
+          © {new Date().getFullYear()} Robert Lieblang
+        </p>
+
         <a
-          href="https://github.com/roblieblang"
+          className="text-xs text-white"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer noopener"
+          href="https://docs.google.com/document/d/15vZrsNrQYvpYGiOf0ifh95p6uoQIfcIK91kzO_SjO6k/edit?usp=sharing"
         >
-          <FaGithub size={25} />
+          Privacy Policy
         </a>
-        <a
-          href="https://linkedin.com/in/roblieblang"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin size={25} />
-        </a>
+        <div className="flex space-x-2 lg:scale-125 lg:ml-4">
+          <a
+            href="https://github.com/roblieblang"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={15} />
+          </a>
+          <a
+            href="https://linkedin.com/in/roblieblang"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={15} />
+          </a>
+        </div>
       </div>
     </footer>
   );
